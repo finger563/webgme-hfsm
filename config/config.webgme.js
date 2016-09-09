3490080863cc2c03f720056e5e624f3c54464d2c
William Emfinger <waemfinger@gmail.com>
2016-09-09 13:36:20 -0500

    updating meta to heirarchichal FSM, with attributes and model objects for BG script generation.

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -6,17 +6,28 @@
 var config = require('webgme/config/config.default'),
     validateConfig = require('webgme/config/validator');
 
-
 // The paths can be loaded from the webgme-setup.json
-config.plugin.basePaths.push('src/plugins');
-config.visualization.decoratorPaths.push('src/decorators');
-config.seedProjects.basePaths.push('src/seeds/FiniteStateMachine');
+config.plugin.basePaths.push(__dirname + '/../src/plugins');
+config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
+config.seedProjects.basePaths.push(__dirname + '/../src/seeds/FiniteStateMachine');
+
+
+
+config.visualization.panelPaths.push(__dirname + '/../node_modules/webgme-codeeditor/src/visualizers/panels');
+
+
 
 
 // Visualizer descriptors
-
+config.visualization.visualizerDescriptors.push(__dirname + '/../src/visualizers/Visualizers.json');
 // Add requirejs paths
-
+config.requirejsPaths = {
+  'CodeEditor': 'panels/CodeEditor/CodeEditorPanel',
+  'panels': './src/visualizers/panels',
+  'widgets': './src/visualizers/widgets',
+  'panels/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/panels/CodeEditor',
+  'widgets/CodeEditor': './node_modules/webgme-codeeditor/src/visualizers/widgets/CodeEditor'
+};
 
 
 config.mongo.uri = 'mongodb://127.0.0.1:27017/finite_state_machine';
