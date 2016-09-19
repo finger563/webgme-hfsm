1b1688296f1e14950714ad31f3ae070332ee46a2
William Emfinger <waemfinger@gmail.com>
2016-09-19 16:26:03 -0500

    bugfix for server starting.

--- config/config.default.js
+++ config/config.default.js
@@ -3,6 +3,6 @@
 var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
-config.seedProjects.defaultProject = 'FiniteStateMachine';
+//config.seedProjects.defaultProject = 'FiniteStateMachine';
 validateConfig(config);
 module.exports = config;
