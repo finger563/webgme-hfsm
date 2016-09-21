205847a500cac107ba7cd0094a914e664e1cc685
William Emfinger <waemfinger@gmail.com>
2016-09-21 14:19:33 -0500

    updated config to enable users and authentication.

--- config/config.default.js
+++ config/config.default.js
@@ -3,10 +3,15 @@
 var config = require('./config.webgme'),
     validateConfig = require('webgme/config/validator');
 
+config.server.port = 8888;
+
 //config.seedProjects.defaultProject = 'FiniteStateMachine';
 config.seedProjects.basePaths = [__dirname + '/../src/seeds'];
 
 config.requirejsPaths.hfsm = "./src/common";
+
+config.authentication.enable = true;
+config.authentication.allowGuests = true;
 
 config.plugin.allowBrowserExecution = true;
 config.plugin.allowServerExecution = true;
