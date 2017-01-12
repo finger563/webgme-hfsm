b2bb6ed51080e8faa6f202f07928f483d47409b1
William Emfinger <waemfinger@gmail.com>
2017-01-12 17:23:08 +0000

    updated to fix bug where the period of the first state wasnt getting set at the proper value.

--- src/plugins/GenerateBLE113/templates/c/src/main.c
+++ src/plugins/GenerateBLE113/templates/c/src/main.c
@@ -104,12 +104,13 @@ void main(void)
 
     // generated initialization code for the state machine:
     changeState = 0;
+    stateDelay  = <%- parseInt(parseFloat(model.initState.timerPeriod) * 32768.0) %>;
     // STATE::<%- model.initState.name %>
 <%- model.initStateCode %>
     // execute the init transition for the chart (including user initialization code)
 <%- model.initFunc %>  
     // Start the state timer
-    task_send_timed((enum task_id)task_id_timer_update, 0, 1, <%- parseInt(parseFloat(model.initState.timerPeriod) * 32768.0) %>);
+    task_send_timed((enum task_id)task_id_timer_update, 0, 1, stateDelay);
     
     // start advertisement mode for discoverable/connectable
     // gap_set_mode(gap_general_discoverable, gap_undirected_connectable);
