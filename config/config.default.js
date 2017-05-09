ff9a9a7808636820e7a49527d4fa2cc832cb25b4
William Emfinger <waemfinger@gmail.com>
2017-05-09 14:49:39 -0700

    updated to have svgs for the base seed in the meta.

--- config/config.default.js
+++ config/config.default.js
@@ -8,14 +8,23 @@ config.server.port = 8081;
 //config.seedProjects.defaultProject = 'FiniteStateMachine';
 config.seedProjects.basePaths = [__dirname + '/../src/seeds'];
 
+// requirejs config
 config.requirejsPaths.hfsm = "./src/common";
 config.requirejsPaths['webgme-to-json'] = "./node_modules/webgme-to-json";
 
+// Merging config
+config.storage.autoMerge.enable = true;
+
+// authentication
 config.authentication.enable = true;
 config.authentication.allowGuests = true;
 
 config.plugin.allowBrowserExecution = true;
 config.plugin.allowServerExecution = true;
+
+// svg paths
+var path = require('path');
+config.visualization.svgDirs = [path.join(__dirname, '..', "./src/svgs")];
 
 validateConfig(config);
 module.exports = config;
