474e1fbf42a4de0311e35b9ff008e85bf16a2dbf
William Emfinger <waemfinger@gmail.com>
2017-06-19 14:07:06 -0700

    updated to use bower instead of npm for cytoscape. added cytoscape-edgehandles to allow dragging to create new edges. updated highlighting and such for hfsmviz.

--- config/config.default.js
+++ config/config.default.js
@@ -11,8 +11,6 @@ config.seedProjects.basePaths = [__dirname + '/../src/seeds'];
 // requirejs config
 config.requirejsPaths.hfsm = "./src/common";
 config.requirejsPaths['webgme-to-json'] = "./node_modules/webgme-to-json";
-config.requirejsPaths['cytoscape'] = "./node_modules/cytoscape/dist";
-config.requirejsPaths['cytoscape-cose-bilkent'] = "./node_modules/cytoscape-cose-bilkent/";
 
 // Merging config
 config.storage.autoMerge.enable = true;
--- src/visualizers/widgets/HFSMViz/HFSMVizWidget.js
+++ src/visualizers/widgets/HFSMViz/HFSMVizWidget.js
@@ -7,22 +7,25 @@
 
 define([
     'text!./HFSM.html',
-    'cytoscape/cytoscape',
-    'cytoscape-cose-bilkent/cytoscape-cose-bilkent',
+    './bower_components/cytoscape/dist/cytoscape.min',
+    './bower_components/cytoscape-edgehandles/cytoscape-edgehandles',
+    './bower_components/cytoscape-cose-bilkent/cytoscape-cose-bilkent',
     'text!./style2.css',
     'text!../../../decorators/UMLStateMachineDecorator/Core/highlightjs.default.min.css',
     'q',
     'css!./styles/HFSMVizWidget.css'], function (
 	HFSMHtml,
 	cytoscape,
+	edgehandles,
 	regCose,
 	styleText,
 	hljsStyleText,
 	Q) {
 	'use strict';
 
+	edgehandles( cytoscape, _.debounce.bind( _ ), _.throttle.bind( _ ) );
 	regCose( cytoscape );
-	
+
 	var HFSMVizWidget,
             WIDGET_CLASS = 'h-f-s-m-viz';
 
@@ -160,55 +163,71 @@ define([
 	    this._cytoscape_options.layout = self._layout_options;
 	    this._cy = cytoscape(self._cytoscape_options);
 
+	    // the default values of each option are outlined below:
+	    var defaults = {
+		preview: true, // whether to show added edges preview before releasing selection
+		stackOrder: 4, // Controls stack order of edgehandles canvas element by setting it's z-index
+		handleSize: 1, // the size of the edge handle put on nodes
+		handleHitThreshold: 3, // a threshold for hit detection that makes it easier to grab the handle
+		handleIcon: false, // an image to put on the handle
+		handleColor: '#ff0000', // the colour of the handle and the line drawn from it
+		handleLineType: 'ghost', // can be 'ghost' for real edge, 'straight' for a straight line, or 'draw' for a draw-as-you-go line
+		handleLineWidth: 1, // width of handle line in pixels
+		handleOutlineColor: '#000000', // the colour of the handle outline
+		handleOutlineWidth: 0, // the width of the handle outline in pixels
+		handleNodes: 'node', // selector/filter function for whether edges can be made from a given node
+		handlePosition: 'middle top', // sets the position of the handle in the format of "X-AXIS Y-AXIS" such as "left top", "middle top"
+		hoverDelay: 150, // time spend over a target node before it is considered a target selection
+		cxt: true, // whether cxt events trigger edgehandles (useful on touch)
+		enabled: false, // whether to start the plugin in the enabled state
+		toggleOffOnLeave: true, // whether an edge is cancelled by leaving a node (true), or whether you need to go over again to cancel (false; allows multiple edges in one pass)
+		edgeType: function( sourceNode, targetNode ) {
+		    // can return 'flat' for flat edges between nodes or 'node' for intermediate node between them
+		    // returning null/undefined means an edge can't be added between the two nodes
+		    return 'flat';
+		},
+		loopAllowed: function( node ) {
+		    // for the specified node, return whether edges from itself to itself are allowed
+		    return true;
+		},
+		nodeLoopOffset: -50, // offset for edgeType: 'node' loops
+		nodeParams: function( sourceNode, targetNode ) {
+		    // for edges between the specified source and target
+		    // return element object to be passed to cy.add() for intermediary node
+		    return {};
+		},
+		edgeParams: function( sourceNode, targetNode, i ) {
+		    // for edges between the specified source and target
+		    // return element object to be passed to cy.add() for edge
+		    // NB: i indicates edge index in case of edgeType: 'node'
+		    return {};
+		},
+		start: function( sourceNode ) {
+		    // fired when edgehandles interaction starts (drag on handle)
+		},
+		complete: function( sourceNode, targetNodes, addedEntities ) {
+		    // fired when edgehandles is done and entities are added
+		},
+		stop: function( sourceNode ) {
+		    // fired when edgehandles interaction is stopped (either complete with added edges or incomplete)
+		}, 
+		cancel: function( sourceNode, renderedPosition, invalidTarget ){
+		    // fired when edgehandles are cancelled ( incomplete - nothing has been added ) - renderedPosition is where the edgehandle was released, invalidTarget is
+		    // a collection on which the handle was released, but which for other reasons (loopAllowed | edgeType) is an invalid target
+		}
+	    };
+
+	    this._cy.edgehandles( defaults );	    
+
 	    var layoutPadding = 50;
 	    var layoutDuration = 500;
 
 	    function highlight( node ){
 		var nhood = node.closedNeighborhood();
 
-		self._cy.batch(function(){
-		    self._cy.elements("edge").not( nhood ).removeClass('highlighted').addClass('faded');
-		    self._cy.elements("node").not( nhood ).removeClass('highlighted').addClass('faded');
-		    nhood.removeClass('faded').addClass('highlighted');
-		    
-		    var npos = node.position;
-		    var w = window.innerWidth;
-		    var h = window.innerHeight;
-
-		    self._cy.stop().animate({
-			fit: {
-			    eles: self._cy.elements(),
-			    padding: layoutPadding
-			}
-		    }, {
-			duration: layoutDuration
-		    }).delay( layoutDuration, function(){
-			nhood.layout({
-			    name: 'concentric',
-			    padding: layoutPadding,
-			    animate: true,
-			    animationDuration: layoutDuration,
-			    boundingBox: {
-				x1: npos.x - w/2,
-				x2: npos.x + w/2,
-				y1: npos.y - w/2,
-				y2: npos.y + w/2
-			    },
-			    fit: true,
-			    concentric: function( n ){
-				if( node.id() === n.id() ){
-				    return 2;
-				} else {
-				    return 1;
-				}
-			    },
-			    levelWidth: function(){
-				return 1;
-			    }
-			});
-		    } );
-		    
-		});
+		self._cy.elements("edge").removeClass('highlighted');
+		self._cy.elements("node").removeClass('highlighted');
+		node.addClass('highlighted');
 	    }
 
 	    function clear(){
@@ -223,19 +242,18 @@ define([
 		});
 	    }
 
-	    self._cy.on('free', 'node', function( e ){
-		var n = e.target;
-		var p = n.position;
-		
-		n.data('orgPos', {
-		    x: p.x,
-		    y: p.y
-		});
-	    });
-
-	    self._cy.on('add', _.debounce(self.reLayout.bind(self), 250));
+	    //self._cy.on('add', _.debounce(self.reLayout.bind(self), 250));
+	    self.debouncedReLayout = _.debounce(self.reLayout.bind(self), 250);
 	    
 	    self._cy.on('select', 'node', function(e){
+		var node = this;
+		if (node.id()) {
+		    WebGMEGlobal.State.registerActiveSelection([node.id()]);
+		}
+		highlight( node );
+	    });
+	    
+	    self._cy.on('select', 'edge', function(e){
 		var node = this;
 		if (node.id()) {
 		    WebGMEGlobal.State.registerActiveSelection([node.id()]);
@@ -275,7 +293,11 @@ define([
 
 	    self._cy.on('unselect', 'node', function(e){
 		var node = this;
+		clear();
+	    });
 
+	    self._cy.on('unselect', 'edge', function(e){
+		var node = this;
 		clear();
 	    });
 
@@ -434,6 +456,7 @@ define([
 	    self._cy.add(node);
 	    self.nodes[desc.id] = desc;
 	    self.updateDependencies();
+	    self.debouncedReLayout();
 	};
 	
 	// Adding/Removing/Updating items
