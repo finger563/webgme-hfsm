10c4c80a8ba66b774726cc2257f3b4ede235749e
William Emfinger <waemfinger@gmail.com>
2017-07-12 07:37:07 -0700

    removed old templates and updated svg.

--- src/plugins/SoftwareGenerator/templates/forEach/Comp.cpp
+++
@@ -1,3 +0,0 @@
-#include "{{{name}}}.hpp"
-
-{{{Definitions}}}
--- src/plugins/SoftwareGenerator/templates/forEach/Comp.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __{{{name}}}_INCLUDE_GUARD__
-#define __{{{name}}}_INCLUDE_GUARD__
-
-{{{Declarations}}}
-
-#endif // __{{{name}}}_INCLUDE_GUARD__
