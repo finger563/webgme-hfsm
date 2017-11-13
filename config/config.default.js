736071c55d7774e88261a71e7a3b4298007c8ebc
William Emfinger <waemfinger@gmail.com>
2017-11-13 06:31:38 -0800

    moved handlebars and mustache over to bower.

--- config/config.default.js
+++ config/config.default.js
@@ -12,10 +12,10 @@ config.seedProjects.basePaths = [__dirname + '/../src/seeds'];
 
 // requirejs config
 config.requirejsPaths.hfsm = "./src/common";
-config.requirejsPaths['mustache'] = "./node_modules/mustache";
-config.requirejsPaths['handlebars'] = "./node_modules/handlebars/dist";
 config.requirejsPaths['webgme-to-json'] = "./node_modules/webgme-to-json";
 config.requirejsPaths['bower'] = "./bower_components/";
+config.requirejsPaths['mustache'] = "bower/mustache";
+config.requirejsPaths['handlebars'] = "./bower_components/handlebars/dist";
 config.requirejsPaths['cytoscape-edgehandles'] = "./bower_components/cytoscape-edgehandles/cytoscape-edgehandles";
 config.requirejsPaths['cytoscape-context-menus'] = "./bower_components/cytoscape-context-menus/cytoscape-context-menus";
 config.requirejsPaths['cytoscape-panzoom'] = "./bower_components/cytoscape-panzoom/cytoscape-panzoom";
