d83d0069690a6de39f5757946677d292df48a235
William Emfinger <waemfinger@gmail.com>
2021-07-16 13:20:47 -0500

    clang format generated states hpp.

--- src/plugins/SoftwareGenerator/templates/forEach/uml/GeneratedStates.hpp
+++ src/plugins/SoftwareGenerator/templates/forEach/uml/GeneratedStates.hpp
@@ -1,73 +1,74 @@
 #ifndef __GENERATED_STATES_INCLUDE_GUARD__
 #define __GENERATED_STATES_INCLUDE_GUARD__
 
-#include "StateBase.hpp"
 #include "DeepHistoryState.hpp"
 #include "ShallowHistoryState.hpp"
+#include "StateBase.hpp"
 
 // User Includes for the HFSM
 //::::{{{path}}}::::Includes::::
-{{{Includes}}}
+{{{ Includes }}}
 
 namespace StateMachine {
 
-  // ROOT OF THE HFSM
-  class {{{sanitizedName}}} : public StateMachine::StateBase {
-  public:
+// ROOT OF THE HFSM
+class {{{sanitizedName}}} : public StateMachine::StateBase {
+public:
+  // User Declarations for the HFSM
+  //::::{{{path}}}::::Declarations::::
+  {{{Declarations}}}
 
-    // User Declarations for the HFSM
-    //::::{{{path}}}::::Declarations::::
-    {{{Declarations}}}
+  // Child Substates
+  {{#each Substates}}
+  {{> StateTemplHpp}}
+  {{/each}}
+  {{#END}}
+  // END STATE
+  {{> EndStateTemplHpp}}
+  {{~/END}}
 
-    // Child Substates
-    {{#each Substates}}
-    {{> StateTemplHpp }}
-    {{/each}}
-    {{#END}}
-    // END STATE
-    {{> EndStateTemplHpp}}
-    {{~/END}}
+  // Constructors
+  {{{sanitizedName}}}(void)
+      : StateBase(){}
+  {{{sanitizedName}}}(StateBase * _parent)
+      : StateBase(_parent) {}
+  ~{{{sanitizedName}}}(void) {}
 
-    // Constructors
-    {{{sanitizedName}}}  ( void ) : StateBase( ) {}
-    {{{sanitizedName}}}  ( StateBase* _parent ) : StateBase( _parent ) {}
-    ~{{{sanitizedName}}} ( void ) {}
-    
-    /**
-     * @brief Fully initializes the HFSM. Runs the HFSM Initialization
-     *  code from the model, then sets the inital state and runs the
-     *  initial transition and entry actions accordingly.
-     */
-    void                     initialize ( void );
-    
-    /**
-     * @brief Terminates the HFSM, calling exit functions for the
-     *  active leaf state upwards through its parents all the way to
-     *  the root.
-     */
-    void                     terminate  ( void );
+  /**
+   * @brief Fully initializes the HFSM. Runs the HFSM Initialization
+   *  code from the model, then sets the inital state and runs the
+   *  initial transition and entry actions accordingly.
+   */
+  void initialize(void);
 
-    /**
-     * @brief Restarts the HFSM by calling terminate and then
-     *  initialize.
-     */
-    void                     restart    ( void );
+  /**
+   * @brief Terminates the HFSM, calling exit functions for the
+   *  active leaf state upwards through its parents all the way to
+   *  the root.
+   */
+  void terminate(void);
 
-    /**
-     * @brief Returns true if the HFSM has reached its END State
-     */
-    bool                     hasStopped ( void );
+  /**
+   * @brief Restarts the HFSM by calling terminate and then
+   *  initialize.
+   */
+  void restart(void);
 
-    /**
-     * @brief Calls handleEvent on the activeLeaf.
-     *
-     * @param[in] StateMachine::Event* Event needing to be handled
-     *
-     * @return true if event is consumed, false otherwise
-     */
-    bool                     handleEvent ( StateMachine::Event* event );
-  };
+  /**
+   * @brief Returns true if the HFSM has reached its END State
+   */
+  bool hasStopped(void);
+
+  /**
+   * @brief Calls handleEvent on the activeLeaf.
+   *
+   * @param[in] StateMachine::Event* Event needing to be handled
+   *
+   * @return true if event is consumed, false otherwise
+   */
+  bool handleEvent(StateMachine::Event * event);
 };
+}; // namespace StateMachine
 
 // pointers
 extern StateMachine::{{{sanitizedName}}} *const {{{sanitizedName}}}_root;
